45a93641929939f6172733c6946c4781c15fc7f1
Julio Antonio Ortiz Pol <j.ortiz@nexus.com.do>
2021-09-06 17:50:42 -0400

    add redux dependencies

--- tcit-posts-app/src/index.js
+++ tcit-posts-app/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from './redux'
+
+
+//STORE
+
+//ACTION
+
+//REDUCER
+
+//DISPATCH
+
+
 
 ReactDOM.render(
   <React.StrictMode>
